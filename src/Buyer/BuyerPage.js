c684f75f3b6b7f92216892280adb231d43adef12
James Baldwin <jamescbaldwin@gmail.com>
2021-06-03 17:39:56 -0400

    directory setup

---
+++ src/Buyer/BuyerPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BuyerPage = () => {};
+
+export default BuyerPage;
---
+++ src/Seller/SellerPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SellerPage = () => {};
+
+export default SellerPage;

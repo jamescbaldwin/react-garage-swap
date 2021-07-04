7caceb378727f28c4c079d15e6f760730a9c5160
Casey Overton <68711930+CaseyOverton@users.noreply.github.com>
2021-07-04 02:13:45 -0400

    added inline styling to sellerform page, using makeStyles! I think this will be beneficial moving forward. Also working on form with material ui, state isnt fully attached and functional but it is a work in progress

--- src/Seller/SellerForm.js
+++ src/Seller/SellerForm.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import MultiSelect from "react-multi-select-component";
-import Select from 'react-select';
+import { makeStyles } from '@material-ui/core/styles';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
 const SellerForm = () => {
   const options = [
     { value: "Apparel", label: "Apparel", id: 1 },
@@ -14,42 +23,79 @@ const SellerForm = () => {
     { value: "Sporting Goods", label: "Sporting Goods", id: 9 },
     { value: "Toys & Toddler", label: "Toys & Toddler", id: 10 },
   ];
- 
+  
+  const classes = useStyles();
+  // const [values, setValues] = React.useState({
+  //   amount: '',
+  //   password: '',
+  //   weight: '',
+  //   weightRange: '',
+  //   showPassword: false,
+  // });
 
-  const [selected, setSelected] = useState([]);
+  const [state, setState] = useState({
+   age: '',
+    name: 'casey'
+  });
 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
   return (
-    <div className='sellerForm'>
-        <form>
-            <label className='formName'>
-                Item Name
-                <input type="text" name="name" />
-             </label>
+    <div className={classes.sellerForm}>
+        <FormControl className={classes.margin} >
 
-            <label className='itemPrice'>
-                Item Price
-             <input type="number" />
-            </label>
-              <div className='dropDown'>
-                <Select
-                options={options}
-                singleSelect={true}
-                theme={theme => ({
-                  ...theme,
-                  borderRadius: 0,
-                  colors: {
-                    ...theme.colors,
-                    primary25: '#179c79',
-                    primary: '#179c79',
-                  }})}
-                 />
-              </div>
+        <TextField id="standard-basic" label="Item Name" />
 
+        <Select
+        className={classes.select} 
+          native
+          value={state.category}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'age-native-simple',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option>Apparel</option>
+          <option>Automotive</option>
+          <option>Electronics</option>
+          <option>Gaming</option> 
+          <option>Jewlry</option>
+          <option>Miscellaneous</option>
+          <option>Sporting Goods</option>
+          <option>Toys & Toddler</option>
+        </Select>
 
-         </form>
-
-    </div>
+        <TextField id="standard-basic" label="Item Price" />
+        <TextField id="standard-basic" label="Another Category" />
+        <TextField id="standard-basic" label="And Another Category" />
+            {/* <Input
+            className={classes.numberInput}
+            value={values.amount}
+            onChange={handleChange('amount')}
+            startAdornment={<InputAdornment position="end">$</InputAdornment>}
+          /> */}
+        </FormControl>
+        </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  sellerForm: {
+    maxWidth: "40%"
+  },
+  select: {
+padding: 10,
+  }
+}));
 
 export default SellerForm;
